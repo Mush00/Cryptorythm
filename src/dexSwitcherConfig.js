42b8cca0a46406f85e5ef4346c4df809bcecd0a2
Mush00 <Angelo007@live.com>
2025-06-18 04:48:40 +0100

    Update dexSwitcherConfig.js

--- src/dexSwitcherConfig.js
+++ src/dexSwitcherConfig.js
@@ -1,14 +1,13 @@
-
 export const dexConfig = {
   defaultDEX: "pancakeswap",
   supportedDEXs: {
     pancakeswap: {
       rpc: "https://bsc-dataseed.binance.org/",
-      router: "0x10ED43C718714eb63d5aA57B78B54704E256024E"
+      router: "0x10ED43C718714eb63d5aA57B78B54704E256024E",
     },
     uniswap: {
-      rpc: "https://mainnet.infura.io/v3/YOUR_PROJECT_ID",
-      router: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+      rpc: "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID",
+      router: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
     }
   }
 };
