4324ddc139fcdccf228b52b52b3a2ef530fcc81a
Mush00 <Angelo007@live.com>
2025-06-18 05:54:03 +0100

    Update growthCampaignManager.js

--- src/growthCampaignManager.js
+++ src/growthCampaignManager.js
@@ -1,15 +1,23 @@
+// growthCampaignManager.js
 
 const fs = require('fs');
 
+/**
+ * Generates a growth campaign object for an NFT.
+ * @param {string} nftName - The name of the NFT.
+ * @param {string} rarity - The rarity level.
+ * @returns {object} campaign - Campaign metadata.
+ */
 function generateCampaign(nftName, rarity) {
   const campaign = {
     title: `🌟 ${nftName} Release`,
-    description: `${nftName} is a new ultra-${rarity} NFT added this month.`,
-    media: `/assets/nft/${nftName.replace(/\s+/g, '_')}.png`,
+    description: `${nftName} is a new ultra-rare ${rarity} NFT.`,
+    media: `/assets/nft/${nftName.replace(/\s+/g, '_').toLowerCase()}.png`,
     channels: ["Twitter", "Discord", "Newsletter"]
   };
+
   console.log("📦 Campaign Created:", campaign);
   return campaign;
 }
 
-generateCampaign("Quantum Prism Relic", "rare");
+module.exports = { generateCampaign };
