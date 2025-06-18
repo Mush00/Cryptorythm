ffc0a541ca00063b348dec31a378ea7d776b93f9
Mush00 <Angelo007@live.com>
2025-06-18 06:12:03 +0100

    Update dexSwitcherConfig.js

--- src/dexSwitcherConfig.js
+++ src/dexSwitcherConfig.js
@@ -1,13 +1,19 @@
+/**
+ * 🔁 DEX Configuration Switcher
+ * Centralized config file to dynamically switch between supported DEXes
+ * 🔄 Expandable: Add SushiSwap, QuickSwap, etc. here later
+ */
+
 export const dexConfig = {
-  defaultDEX: "pancakeswap",
+  defaultDEX: "pancakeswap", // ✳️ Used as fallback/default
   supportedDEXs: {
     pancakeswap: {
-      rpc: "https://bsc-dataseed.binance.org/",
-      router: "0x10ED43C718714eb63d5aA57B78B54704E256024E",
+      rpc: "https://bsc-dataseed.binance.org/", // 🟡 BSC Node
+      router: "0x10ED43C718714eb63d5aA57B78B54704E256024E" // 🥞 PancakeSwap Router
     },
     uniswap: {
-      rpc: "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID",
-      router: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+      rpc: "https://mainnet.infura.io/v3/YOUR_INFURA_KEY", // 🔵 Ethereum Node
+      router: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D" // 🦄 Uniswap V2 Router
     }
   }
 };
